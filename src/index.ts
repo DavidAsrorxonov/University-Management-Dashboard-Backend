52c6df07b140bc9515e37e7c4a36583f113f2df1
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-18 12:58:38 +0900

    feat: Validate presence of FRONTEND_URL environment variable.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 
 const app = express();
 
+if (!process.env.FRONTEND_URL) {
+  throw new Error("FRONTEND_URL is not defined");
+}
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
